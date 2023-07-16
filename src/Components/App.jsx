5e40d7578ac1333a6dc5db04019f2112172b5c88
faizan alam <alamf6023@gmail.com>
2023-07-16 22:39:15 +0530

    build: create room page with design and logic

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,15 +1,26 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom"
 import HomePage from "./Screens/HomePage"
-import Menu from "./Menu"
+// import Menu from "./Menu"
 import ErrorPage from "./Screens/ErrorPage"
 import About from "./Screens/About"
 import Contact from "./Screens/Contact"
 import CodeEditor from "./Screens/CodeEditor"
-
+import Editor from "./Screens/Collaborative/Editor"
+import Room from "./Screens/Collaborative/room"
+import { Toaster } from "react-hot-toast"
 
 export default function App() {
   return (
     <>
+    <div>
+      <Toaster position="top-right" toastOptions={{
+        success:{
+          theme:{
+            primary:'#4aed88'
+          }
+        }
+      }}></Toaster>
+    </div>
     <BrowserRouter>
     {/* <Menu/> */}
     <Routes>
@@ -18,6 +29,8 @@ export default function App() {
       <Route path="contact" element={<Contact/>}/>
       <Route path="codeeditor" element={<CodeEditor/>}/>
       <Route path="*" element={<ErrorPage/>}/>
+      <Route path="room" element={<Room/>}></Route>
+      <Route path="/editor/:roomId" element={<Editor/>}></Route>
     </Routes>
     </BrowserRouter>
     </>
--- src/Components/Screens/Collaborative/room.jsx
+++ src/Components/Screens/Collaborative/room.jsx
@@ -1,6 +1,57 @@
+import { useState } from "react";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { v4 as uuidV4 } from "uuid";
 
-export default function room() {
+
+export default function Room() {
+
+    const navigate = useNavigate();
+    const [roomId,setRoomId] = useState('');
+    const [username,setusername] = useState('')
+
+    const createNewRoom = (e)=>{
+        e.preventDefault();
+        const id = uuidV4();
+        setRoomId(id)
+        // console.log(id)
+        toast.success('Created a new room')
+    }
+
+    const joinRoom = ()=>{
+        if(!roomId || !username){
+            toast.error('Room ID & UserName is Required')
+            return;
+        }
+        //Redirect
+        navigate(`/editor/${roomId}`,{
+            state: {
+                // userName : username
+                username,
+            }
+        })
+    }
+
   return (
-    <div>room</div>
+    <>
+        <div className="homePageWrapper">
+            <div className="formWrapper">
+                <img className="homePageLogo" src="/hacker.jpeg" alt="Logo" />
+                <h4 className="mainLabel">Paste Invitation Room Id</h4>
+                <div className="inputGroup">
+                    <input type="text" className="inputBox" placeholder="ROOM ID" onChange={(e)=>setRoomId(e.target.value)} value={roomId} />
+                    <input type="text" className="inputBox" placeholder="USERNAME" onChange={(e)=>setusername(e.target.value)} value={username} />
+                    <button className="btn joinBtn" onClick={joinRoom}>JOIN</button>
+                    <span className="createInfo">
+                        If You dont have an invite then create &nbsp;
+                        <a onClick={createNewRoom} href="" className="createNewBtn">NEW ROOM</a>
+                    </span>
+                </div>
+            </div>
+            <footer>
+                <h4>Build with 💛 &nbsp; By <a href="">Faizan Alam</a></h4>
+            </footer>
+        </div>
+    </>
   )
 }
